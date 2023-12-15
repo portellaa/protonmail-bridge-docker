8ebd77d4a19455365e6e58c5bb06970a4f2c2266
Luís Portela Afonso <lcportellaa@gmail.com>
2023-12-15 10:30:33 +0000

    fix entrypoint

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -13,7 +13,7 @@ if [[ $1 == init ]]; then
     # This allows users to run entrypoint init inside a running conainter
     # which is useful in a k8s environment.
     # || true to make sure this would not fail in case there is no running instance.
-    killall protonmail-bridge || true
+    kill `pidof /protonmail/bridge` || true
 
     # Login
     /protonmail/proton-bridge --cli $@
